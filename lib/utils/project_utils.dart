0c822db5bef61a594f51a698604d1de47fcaec73
emmalexander <32887883+emmalexander@users.noreply.github.com>
2023-08-28 13:17:39 +0100

    portfolio changes

--- lib/utils/project_utils.dart
+++ lib/utils/project_utils.dart
@@ -1,41 +1,41 @@
 class ProjectUtils {
   static const List<String> banners = [
-    "assets/projects/SimpleCalculatorB.png",
-    "assets/projects/TodoeyB.png",
-    "assets/projects/ExploreB.png",
-    "assets/projects/TravelAppB.png",
-    "assets/projects/WeatherAppB.png",
+    "assets/projects/Ardilla.png",
+    "assets/projects/IntercityBusiness.png",
+    "assets/projects/Explore.png",
+    "assets/projects/QuickChop.png",
+    "assets/projects/WaterReminderApp.png",
   ];
 
   static const List<String> icons = [
+    "assets/projects/ArdillaLogo.png",
+    "assets/projects/IntercityBusinessLogo.png",
     "assets/projects/flutter.png",
-    "assets/projects/flutter.png",
-    "assets/projects/flutter.png",
-    "assets/projects/flutter.png",
+    "assets/projects/qc-logo.png",
     "assets/projects/flutter.png",
   ];
 
   static const List<String> titles = [
-    "A Simple Calculator",
-    "Todoey",
+    "Ardilla UI Mockup",
+    "Intercity Business",
     "Explore",
-    "Travel App",
-    "Weather App",
+    "Quick Chop",
+    "Water Reminder App",
   ];
 
   static const List<String> description = [
-    "A simple Calculator with dark and light mode support.",
-    "A todo app made with Provider state management.",
+    "A UI Mockup for Ardilla Payment Interface.",
+    "Sell Interstate transport bus tickets and earn real rewards in cash.",
     "This app displays a long list of every country, their capitals and their flags as provided by the REST Country API.",
-    "Discover the different locations of the world and various information about these locations and the ratings by past visitors.",
-    "Get the current weather of your favorite places in the world.",
+    "This is a food delivery app built using Flutter and Bloc architecture, which allows users to order food from their favorite restaurants and have it delivered to their location.",
+    "Get reminded to take water at your own pace.",
   ];
 
   static const List<String> links = [
-    "https://github.com/emmalexander/simple_calculator",
-    "https://github.com/emmalexander/todoey_reo",
+    "https://github.com/emmalexander/ardilla_assessment",
+    "https://play.google.com/store/apps/details?id=com.t40.retailerApp",
     "https://github.com/emmalexander/explore_reo",
-    "https://github.com/emmalexander/travel_app",
-    "https://github.com/emmalexander/weather_app",
+    "https://github.com/emmalexander/delivery_project_app",
+    "https://github.com/emmalexander/water_reminder_app",
   ];
 }
